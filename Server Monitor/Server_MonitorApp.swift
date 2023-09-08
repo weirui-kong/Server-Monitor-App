dfe3436f54a8ba76925c81b3e64a1e4c2f8f1835
孔维锐 <weiruik@outlook.com>
2023-09-08 14:09:33 +0800

    Initial Commit

---
+++ Server Monitor/Server_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Server_MonitorApp.swift
+//  Server Monitor
+//
+//  Created by 孔维锐 on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Server_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
